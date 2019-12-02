8286cf13cba746049d57ac387fedcca52f821484
nodugurru <58426265+nodugurru@users.noreply.github.com>
2019-12-02 07:33:51 -0800

    package changes

--- src/main/java/kalpatech/jenkins/BankAccount.java
+++ src/main/java/kalpatech/jenkins/BankAccount.java
@@ -1,4 +1,4 @@
-package ca.uwo.csd.cs2212.USERNAME;
+package kalpatech.jenkins;
 
 public class BankAccount {
 
--- src/test/java/kalpatech/jenkins/TestBankAccount.java
+++ src/test/java/kalpatech/jenkins/TestBankAccount.java
@@ -1,4 +1,4 @@
-package ca.uwo.csd.cs2212.USERNAME;
+package kalpatech.jenkins;
 
 import junit.framework.Assert;
 import org.junit.Before;
